422613931fe4e3e4a1ac4f5a4c1e78bb1ddc4bba
NataLobster <abra_cadabra_@list.ru>
2022-05-01 09:49:51 +0300

    add_profiling

---
+++ src/forest_cover/prof_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+import click
+from pathlib import Path
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-report-path",
+    default="data/prof.html",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+def prof(
+    dataset_path: Path,
+    save_report_path: Path,
+) -> None:
+    df = pd.read_csv(dataset_path)
+    profile = ProfileReport(df,title='Pandas Profiling Report')
+    profile.to_file(save_report_path)
