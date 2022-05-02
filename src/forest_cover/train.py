f5c2ebcbac55d8a8a2919c96620b0008a690798f
NataLobster <abra_cadabra_@list.ru>
2022-05-02 15:47:57 +0300

    extract data loading to data.py

--- src/forest_cover/train.py
+++ src/forest_cover/train.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
 import click
-import pandas as pd
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+
+from .data import get_dataset
+
 
 @click.command()
 @click.option(
@@ -14,15 +16,20 @@ from sklearn.model_selection import train_test_split
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
+@click.option("--random-state", default=42, type=int)
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+)
 def train(
-    dataset_path: Path
+    dataset_path: Path,random_state: int,
+    test_split_ratio: float
 ) -> None:
-    dataset = pd.read_csv(dataset_path)
-    click.echo(f"Dataset shape: {dataset.shape}.")
-    features = dataset.drop("Cover_Type", axis=1)
-    target = dataset["Cover_Type"]
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=0.2, random_state=42
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_split_ratio,
     )
     classifier = LogisticRegression(random_state=42,  max_iter=5000).fit(features_train, target_train)
     accuracy = accuracy_score(target_val, classifier.predict(features_val))
@@ -35,17 +42,22 @@ def train(
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
+@click.option("--random-state", default=42, type=int)
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+)
+
 def train_rf(
-    dataset_path: Path
+    dataset_path: Path, random_state: int,
+    test_split_ratio: float, 
 ) -> None:
-    dataset = pd.read_csv(dataset_path)
-    click.echo(f"Dataset shape: {dataset.shape}.")
-    features = dataset.drop("Cover_Type", axis=1)
-    target = dataset["Cover_Type"]
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=0.2, random_state=42
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_split_ratio,
     )
     classifier = RandomForestClassifier(random_state=42).fit(features_train, target_train)
     accuracy = accuracy_score(target_val, classifier.predict(features_val))
     click.echo(f"Accuracy: {accuracy}.")
-    
