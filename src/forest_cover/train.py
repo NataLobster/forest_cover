c0ed7df0ea0a7869bbfc57feb9a3bbcc3f0f2169
NataLobster <abra_cadabra_@list.ru>
2022-05-02 16:14:59 +0300

    add saving model

--- src/forest_cover/train.py
+++ src/forest_cover/train.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from joblib import dump
 
 import click
+
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -16,6 +18,13 @@ from .data import get_dataset
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model_lr.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
 @click.option("--random-state", default=42, type=int)
 @click.option(
     "--test-split-ratio",
@@ -23,7 +32,7 @@ from .data import get_dataset
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
 )
 def train(
-    dataset_path: Path,random_state: int,
+    dataset_path: Path,random_state: int, save_model_path: Path,
     test_split_ratio: float
 ) -> None:
     features_train, features_val, target_train, target_val = get_dataset(
@@ -34,6 +43,7 @@ def train(
     classifier = LogisticRegression(random_state=42,  max_iter=5000).fit(features_train, target_train)
     accuracy = accuracy_score(target_val, classifier.predict(features_val))
     click.echo(f"Accuracy: {accuracy}.")
+    dump(classifier, save_model_path)
 
 @click.command()
 @click.option(
@@ -41,6 +51,13 @@ def train(
     "--dataset-path",
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model_rf.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
 )
 @click.option("--random-state", default=42, type=int)
 @click.option(
@@ -50,8 +67,8 @@ def train(
 )
 
 def train_rf(
-    dataset_path: Path, random_state: int,
-    test_split_ratio: float, 
+    dataset_path: Path, random_state: int, save_model_path: Path, 
+    test_split_ratio: float,
 ) -> None:
     features_train, features_val, target_train, target_val = get_dataset(
         dataset_path,
@@ -61,3 +78,4 @@ def train_rf(
     classifier = RandomForestClassifier(random_state=42).fit(features_train, target_train)
     accuracy = accuracy_score(target_val, classifier.predict(features_val))
     click.echo(f"Accuracy: {accuracy}.")
+    dump(classifier, save_model_path)
