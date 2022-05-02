7b84d804254bfd3ecc080910881fecccbe0a294e
NataLobster <abra_cadabra_@list.ru>
2022-05-02 17:02:49 +0300

    extract model creation from train.py

--- src/forest_cover/train.py
+++ src/forest_cover/train.py
@@ -5,10 +5,12 @@ import click
 
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-from .data import get_dataset
+from forest_cover.data import get_dataset
+from forest_cover.pipeline import create_pipeline
 
 
 @click.command()
@@ -31,51 +33,34 @@ from .data import get_dataset
     default=0.2,
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
 )
-def train(
-    dataset_path: Path,random_state: int, save_model_path: Path,
-    test_split_ratio: float
-) -> None:
-    features_train, features_val, target_train, target_val = get_dataset(
-        dataset_path,
-        random_state,
-        test_split_ratio,
-    )
-    classifier = LogisticRegression(random_state=42,  max_iter=5000).fit(features_train, target_train)
-    accuracy = accuracy_score(target_val, classifier.predict(features_val))
-    click.echo(f"Accuracy: {accuracy}.")
-    dump(classifier, save_model_path)
-
-@click.command()
 @click.option(
-    "-d",
-    "--dataset-path",
-    default="data/train.csv",
-    type=click.Path(exists=True, dir_okay=False, path_type=Path)
+    "--classifier",
+    default='KNeighborsClassifier',
+    type=str,
 )
 @click.option(
-    "-s",
-    "--save-model-path",
-    default="data/model_rf.joblib",
-    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    "--use-scaler",
+    default=True,
+    type=bool,
     show_default=True,
 )
-@click.option("--random-state", default=42, type=int)
 @click.option(
-    "--test-split-ratio",
-    default=0.2,
-    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    "--logreg-c",
+    default=1.0,
+    type=float,
+    show_default=True,
 )
-
-def train_rf(
-    dataset_path: Path, random_state: int, save_model_path: Path, 
-    test_split_ratio: float,
+def train(
+    dataset_path: Path, random_state: int, save_model_path: Path,
+    classifier: str, test_split_ratio: float, use_scaler:bool, logreg_c:float
 ) -> None:
     features_train, features_val, target_train, target_val = get_dataset(
         dataset_path,
         random_state,
         test_split_ratio,
     )
-    classifier = RandomForestClassifier(random_state=42).fit(features_train, target_train)
-    accuracy = accuracy_score(target_val, classifier.predict(features_val))
+    pipeline = create_pipeline(use_scaler, classifier, logreg_c, random_state)
+    pipeline.fit(features_train, target_train)
+    accuracy = accuracy_score(target_val, pipeline.predict(features_val))
     click.echo(f"Accuracy: {accuracy}.")
     dump(classifier, save_model_path)
